3de00ef771bc781830880c425513b4055b4be12e
Suraj Das <suraj140202@gmail.com>
2024-10-26 11:49:02 +0530

    Respond with 200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,22 +9,24 @@ public class Main {
 
     // Uncomment this block to pass the first stage
     //
-
+    ServerSocket serverSocket = null;
     Socket clientSocket = null;
 
     try {
-      ServerSocket serverSocket = new ServerSocket(4221);
+      serverSocket = new ServerSocket(4221);
     
       // Since the tester restarts your program quite often, setting SO_REUSEADDR
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
-    
-      serverSocket.accept(); // Wait for connection from client.
-
-      clientSocket = serverSocket.accept(); // Wait for connection from client.
-
-      System.out.println("accepted new connection");
       
+      clientSocket = serverSocket.accept(); // Wait for connection from client.
+      
+      System.out.println("accepted new connection");
+    
+      // serverSocket.accept(); // Wait for connection from client.
+
+
+
       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
 
     } catch (IOException e) {
